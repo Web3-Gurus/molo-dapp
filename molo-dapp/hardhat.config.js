f60dde8ab8cac7b3035d7ff7dbbe49cb44514803
iSommie <anajembaedwin@gmail.com>
2022-11-30 21:13:39 +0100

    added smart contract

--- molo-dapp/hardhat.config.js
+++ molo-dapp/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: "<https://polygon-mumbai.g.alchemy.com/v2/pSYYd7SGqr09sTjoHDHyH3ahOgDlW3yy>",
+      accounts: process.env.PRIVATE_KEY,
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+  },
 };
