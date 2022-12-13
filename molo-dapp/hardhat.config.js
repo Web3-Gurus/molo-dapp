d782d65a5de798e29fac200f7e7d0378d43b64be
iSommie <anajembaedwin@gmail.com>
2022-12-13 20:29:16 +0100

    changes made to hardhat.config.js

--- molo-dapp/hardhat.config.js
+++ molo-dapp/hardhat.config.js
@@ -1,13 +1,22 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "mumbai",
   networks: {
+    hardhat: {},
     mumbai: {
-      url: "<https://polygon-mumbai.g.alchemy.com/v2/pSYYd7SGqr09sTjoHDHyH3ahOgDlW3yy>",
-      accounts: process.env.PRIVATE_KEY,
+      url: "<https://polygon-mumbai.g.alchemy.com/v2/pSYYd7SGqr09sTjoHDHyH3ahOgDlW3yy",
+      accounts: [`0x${PRIVATE_KEY}`],
     },
   },
   paths: {
--- molo-dapp/scripts/deploy.js
+++ molo-dapp/scripts/deploy.js
@@ -7,25 +7,40 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // const Lock = await hre.ethers.getContractFactory("Lock");
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  // await lock.deployed();
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // console.log(
+  //   `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // );
+
+  const Molo = await hre.ethers.getContractFactory("Molo");
+  const molo = await Molo.deploy();
+
+  await molo.deployed();
+
+  console.log("Molo deployed to:", molo.address);
 }
+
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
